1252d470eeafc4c2411338fb9d0bbd737c729bf2
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-07-16 14:28:42 +0200

    add login component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,18 +3,38 @@ import { Signup } from './Signup'
 import { Container } from "react-bootstrap"
 import { AuthProvider } from '../contexts/AuthContext'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Dashboard } from './Dashboard';
+import { Login } from './Login';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard />
+    },
+    {
+      path: "/signup",
+      element: <Signup />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    },
+  ]);
 
   return (
     <>
-      <AuthProvider>
-        <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: "100vh" }}>
-          <div className='w-100' style={{ maxWidth: "400px" }}>
-            <Signup />
-          </div>
-        </Container>
-      </AuthProvider>
+      <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: "100vh" }}>
+        <div className='w-100' style={{ maxWidth: "400px" }}>
+          <AuthProvider>
+            <RouterProvider router={router} />
+          </AuthProvider>
+        </div>
+      </Container>
     </>
   )
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,29 +1,23 @@
+
 import { Card, Form, Button, Alert } from 'react-bootstrap'
 import { useRef, useState } from "react"
-import { useAuth } from "../contexts/AuthContext"
 import { Link } from "react-router-dom";
 
-export function Signup() {
+export function Login() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { signup, currentUser } = useAuth()
     const [error, setError] = useState<string>('')
     const [loading, setLoading] = useState<boolean>(false)
 
     async function handleSubmit(e) {
         e.preventDefault()
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError("password do not match")
-        }
         try {
             setError("")
             setLoading(true)
-            await signup(emailRef.current.value ?? "", passwordRef.current.value ?? "")
 
         } catch {
-            setError("Failed to create an account")
+            setError("Failed to log in")
         } finally {
             setLoading(false)
         }
@@ -33,7 +27,7 @@ export function Signup() {
         <div>
             <Card>
                 <Card.Body>
-                    <h2 className='text-center mb-4'>Sign Up</h2>
+                    <h2 className='text-center mb-4'>Log in</h2>
                     {error && <Alert variant='danger'>{error}</Alert>}
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
@@ -44,18 +38,15 @@ export function Signup() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" required ref={passwordRef} />
                         </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" required ref={passwordConfirmRef} />
-                        </Form.Group>
+
                         <Button disabled={loading} className='w-100 mt-4' type="submit">
-                            Sign Up
+                            Log in
                         </Button>
                     </Form>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? <Link to="/login">Log in</Link>
+                Need an account? <Link to="/signup">Sign up</Link>
             </div>
         </div>
     )
